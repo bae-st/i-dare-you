bdb9ff04830a14a4843459927e76f53dfe422e51
vp-yongwon <yongwonkim@viralpick.co.kr>
2022-09-07 02:44:10 +0900

    [feat] Add util functions.

--- themes/I_DARE_YOU/functions.php
+++ themes/I_DARE_YOU/functions.php
@@ -1,5 +1,6 @@
 <?php
 show_admin_bar( false );
+require_once( 'inc/theo-util.php' );
 require_once( 'inc/custom-nav-menu.php' );
 require_once( 'inc/custom-post-type.php' );
 require_once( 'inc/custom-taxonomy.php' );
